a48919437dd836b0eeaafef3de248abe808871bf
Kholodaet <kholodaet@icloud.com>
2024-07-28 11:00:23 +0300

    1-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,38 @@
-import { Heading } from 'components';
+import { lazy, useEffect } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { Header } from 'components';
+import { getUserInfo } from 'service/opencagedataApi';
+const Home = lazy(() => import('pages/Home'));
+const Rates = lazy(() => import('pages/Rates'));
 
 export const App = () => {
-  return <Heading title="Just do it!" />;
+  useEffect(() => {
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    };
+
+    function success(pos) {
+      getUserInfo(pos.coords);
+    }
+
+    function error(err) {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+
+    navigator.geolocation.getCurrentPosition(success, error, options);
+  }, []);
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Header />}>
+          <Route index element={<Home />} />
+          <Route path="/rates" element={<Rates />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </>
+  );
 };
